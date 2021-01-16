5c1c334e816924857b29fa570e9f3463600597c1
Steven Lowe <ewolsteven@gmail.com>
2021-01-16 15:01:33 +1000

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,11 @@
+import smtplib
+
+SENDER_EMAIL = 'ewolsteven@gmail.com'
+SENDER_PASSWORD = ''
+
+def send_email(receiver_email, subject, body):
+    message = 'Subject: {}\n\n{}'.format(subject, body)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(SENDER_EMAIL, receiver_email, message)
+        
